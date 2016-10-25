7b3b1e0822fedd55b3c5aaddfe72a86bef8b9a18
Martin Lärka <martin.larka@gmail.com>
2016-10-25 14:36:57 +0200

    added reading of body

--- santa.go
+++ santa.go
@@ -2,11 +2,21 @@ package main
 
 import (
         "net/http"
+	"io"
+	"io/ioutil"
 	"fmt"
 )
 
 func calcSantas(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	fmt.Println(body)
 }
 
 func main() {
