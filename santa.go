993b80be3c3dd27be81b1b7c38b92cb1bbe0c3ed
Martin Lärka <martin.larka@gmail.com>
2016-10-25 14:18:53 +0200

    Add function handler

--- santa.go
+++ santa.go
@@ -2,9 +2,15 @@ package main
 
 import (
         "net/http"
+	"fmt"
 )
 
+func calcSantas(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-        http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.HandleFunc("/santa/", calcSantas)
         http.ListenAndServe(":3000", nil)
 }
