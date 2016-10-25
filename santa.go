f48837ee270abf86b70abe66f78f3d82916b518c
Martin Lärka <martin.larka@gmail.com>
2016-10-25 22:34:09 +0200

    Added santa parsing

--- santa.go
+++ santa.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-        "net/http"
+	"net/http"
 	"io"
 	"io/ioutil"
 	"fmt"
+	"encoding/json"
 )
 
 func calcSantas(w http.ResponseWriter, r *http.Request) {
+	var santa []Santa
+	
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
@@ -16,7 +19,14 @@ func calcSantas(w http.ResponseWriter, r *http.Request) {
 	if err := r.Body.Close(); err != nil {
 		panic(err)
 	}
-	fmt.Println(body)
+	if err := json.Unmarshal(body, &santa); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println(santa)
 }
 
 func main() {
